7ad7310f4402a0132d912e0105d8d182784f7507
iryna <irina.panasiuk1@gmail.com>
2020-03-26 11:14:53 +0200

    PageObject

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
@@ -7,11 +8,7 @@ public class TestSuite extends TestBase {
 
     @Test(description = "facebook login page example")
     public void testCaseExample(){
+        Assert.assertEquals(webDriver.getTitle(), "Facebook - Log In or Sign Up");
 
     }
-
-
-
-
-
 }
